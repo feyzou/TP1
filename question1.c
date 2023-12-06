f28fc36bf8f14159f81f94a74d573279f0adf0a9
jean-philippe thomar <jeanthom88@d071-pc7.smbedu.ensea.fr>
2023-12-06 16:31:25 +0100

    question 1

--- question1.c
+++ question1.c
@@ -3,12 +3,6 @@
 int main(int argc, char** argv) {
     write(1, WELCOME,strlen(WELCOME));
     write(1, EXIT, strlen(EXIT));
-    scanf("%s",sString);
-    if (strcmp(sString, "exit")== 0)
-    {
-        exit(0);
-    }
-    else {
-        write(1, "Erreur :( \t Veuillez réessayer.\n",32);
-    }
+    write(1, enseash, strlen(enseash));
+
 }
