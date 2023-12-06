71f5ac122c7b0bd01bfa1beca1494eab558ee7f3
jean-philippe thomar <jeanthom88@d071-pc7.smbedu.ensea.fr>
2023-12-06 17:05:49 +0100

    question2 updated

--- question2.c
+++ question2.c
@@ -4,12 +4,28 @@
 #include "fonctions.h"
 
 int main(void){
-    char commande[MAX_SIZE];
-    int commande_size;
+    char cmd[BUF_SIZE];
+    int cmd_size;
+    int pid;
+    int status;
 
-    write(STDOUT_FILENO, HELLO, strlen(HELLO));
-    write(STDOUT_FILENO, REGULAR_PROMPT, strlen(REGULAR_PROMPT));
+    write(1, WELCOME,strlen(WELCOME));
+    write(1, EXIT, strlen(EXIT));
+    write(1, enseash, strlen(enseash));
 
-    commande_size = read(STDOUT_FILENO, commande, MAX_SIZE);
-    write(STDOUT_FILENO, commande, commande_size);
-}
+    while (1) {
+        cmd_size = read(STDIN_FILENO, cmd, BUF_SIZE - 1); //reading entered command
+        write(STDOUT_FILENO, cmd, strlen((cmd) - 1)); //printing entered command
+        cmd[cmd_size - 1] = 0; //end of message
+        pid = fork();
+        if (pid != 0) {
+            wait(&status);
+
+        } else {
+            execlp(cmd, cmd, (char *) NULL);
+        }
+        write(STDOUT_FILENO, enseash, strlen(enseash));
+    }
+    return 0;
+
+        }
