f9865aef89af03755531868afeba34feb2737622
jean-philippe thomar <jeanthom88@d071-pc7.smbedu.ensea.fr>
2023-12-06 14:34:17 +0100

    test

--- fonctions.h
+++ fonctions.h
@@ -7,5 +7,5 @@ char sString [100];
 #define WELCOME "Bienvenue dans le shell ENSEA.\n"
 #define EXIT "Pour quitter, tapez 'exit'. \n"
 
-#define MAX_SIZE 128
-#define REGULAR PROMPT "enseash % "
+#define BUFFER_SIZE 128
+#define REGULAR_PROMPT "enseash % "
